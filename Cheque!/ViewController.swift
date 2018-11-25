fded960095107cbe0ac5ac239674dc21d6d2220d
Natalie Kalitsi <nsk44@cornell.edu>
2018-11-24 21:59:42 -0500

    Initial Commit

---
+++ Cheque!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cheque!
+//
+//  Created by Natalie Sena Kalitsi on 11/24/18.
+//  Copyright © 2018 Natalie Kalitsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
